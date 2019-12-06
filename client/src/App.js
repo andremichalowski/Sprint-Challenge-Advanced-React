d21dc39dc9738cda41aca0044c31005506a9511a
amichalo421 <andre.michalowski@gmail.com>
2019-12-06 11:24:16 +0100

    Api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
-}
+import axios from 'axios';
+import Players from './components/Players';
 
-export default App;
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+  
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        // console.log(res);
+        this.setState({ players: res.data });
+      })
+      .catch(err => {
+        console.log('data not returned');
+      });
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Worlds Womens Cup
+          </h1>
+          <Players players={this.state.players} />
+        </header>
+      </div>
+    );
+  }
+}
